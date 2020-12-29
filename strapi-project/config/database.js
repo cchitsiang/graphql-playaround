6e573a9ffa727b7db754a6f03ce5726997cc1710
Chew <cchitsiang@gmail.com>
2020-12-30 02:02:36 +0800

    strapi: move to postgres from sqlite

--- strapi-project/config/database.js
+++ strapi-project/config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
         useNullAsDefault: true,
